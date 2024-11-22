c7e7f1fa48bbffabc9eec61f3b253abec1ae221c
unknown <LeoLesueurPro@gmail.com>
2024-11-22 07:53:31 +0100

    cleaned the code

--- carComponent/car.mjs
+++ carComponent/car.mjs
@@ -7,11 +7,9 @@ car.height = 94; //voiture.png height
 car.width = 200; //voiture.png width
 car.style.height = car.height + 'px';
 car.style.width = car.width + 'px';
-
 car.style.zIndex = "1";
+
 //Sprite
 car.style.backgroundImage = 'url(carComponent/voiture.png)';
-
-//Transition 
 
 export default car;
