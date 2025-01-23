e90c2b2b82a93fbf97f73f8f371783ed5972b0b5
D0see <139717826+D0see@users.noreply.github.com>
2025-01-23 12:01:46 +0100

    Update collisionChecker.mjs
    Added a tolerance value + default tolerance to avoid rounding errors.

--- utils/collisionChecker.mjs
+++ utils/collisionChecker.mjs
@@ -1,4 +1,4 @@
-const collisionChecker = (obj, pointToCheck) => {
+const collisionChecker = (obj, pointToCheck, tolerance = 0.005) => {
     const calculateTriangleArea = (p1, p2, p3) => {
         return 1/2 * Math.abs((p1[0]*(p2[1] - p3[1]) + p2[0]*(p3[1] - p1[1]) + p3[0]*(p1[1] - p2[1])));
     }
@@ -7,7 +7,8 @@ const collisionChecker = (obj, pointToCheck) => {
     obj.hitboxSegments.forEach(segment => {
         totalArea += calculateTriangleArea(pointToCheck, segment[0], segment[1]);
     })
-    return (Math.trunc(totalArea) === Math.trunc(obj.area));  
+    return (Math.trunc(totalArea) >= Math.trunc(obj.area) - Math.trunc(obj.area)*tolerance &&
+            Math.trunc(totalArea) <= Math.trunc(obj.area) + Math.trunc(obj.area)*tolerance);  
 }
 
 export default collisionChecker;
